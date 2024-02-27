b8b7786ebf8fb8ffcdc9428874d019dc0e228a2a
li_wei <2212261902@qq.com>
2024-02-27 16:12:39 +0800

    对worl文件中的文件进行分类

--- src/world/components/camera.js
+++ src/world/components/camera.js
@@ -1,6 +1,12 @@
 import { PerspectiveCamera } from 'three';
 
 function createCamera() {
+  /*
+  fov，或视野：相机的视野有多宽，以度为单位。
+  aspect，或纵横比：场景的宽度与高度的比率。
+  near, 或近剪裁平面：任何比这更靠近相机的东西都是不可见的。
+  far，或远剪裁平面：任何比这更远离相机的东西都是不可见的。
+  */
   const camera = new PerspectiveCamera(
     35, // fov = Field Of View
     1, // aspect ratio (dummy value)
--- src/world/systems/renderer.js
+++ src/world/systems/renderer.js
@@ -2,7 +2,8 @@ import { WebGLRenderer } from 'three';
 
 function createRenderer() {
   const renderer = new WebGLRenderer();
-
+  /*启用物理上正确的光照*/
+  renderer.physicallyCorrectLights = true;
   return renderer;
 }
 
